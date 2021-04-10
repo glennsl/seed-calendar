144a689f057ad94b92722345642151646391261f
glennsl <glenn@slotte.net>
2021-04-10 22:20:53 +0200

    [test] initial "basic" example

--- examples/basic/src/lib.rs
+++ examples/basic/src/lib.rs
@@ -1,23 +1,46 @@
+use chrono::NaiveDate;
 use seed::{prelude::*, *};
 
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model {}
+    Model {
+        start: None,
+        end: None,
+    }
 }
 // MODEL
 
-struct Model {}
+struct Model {
+    start: Option<NaiveDate>,
+    end: Option<NaiveDate>,
+}
 
 // UPDATE
 
-enum Msg {}
+enum Msg {
+    SelectDate(NaiveDate),
+}
 
 #[allow(clippy::needless_pass_by_value)]
-fn update(_msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {}
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::SelectDate(date) => match (model.start, model.end) {
+            (None, None) => model.start = Some(date),
+            (Some(_), None) => model.end = Some(date),
+            (None, Some(_)) => model.start = Some(date),
+            (Some(_), Some(_)) => {
+                model.start = Some(date);
+                model.end = None
+            }
+        },
+    }
+}
 
 // VIEW
 
-fn view(_model: &Model) -> impl IntoNodes<Msg> {
-    div![seed_calendar::calendar()]
+fn view(model: &Model) -> impl IntoNodes<Msg> {
+    div![seed_calendar::view::MonthView::new(2020, 8)
+        .maybe_with_selection(model.start, model.end)
+        .on_click(Msg::SelectDate)]
 }
 
 // START
